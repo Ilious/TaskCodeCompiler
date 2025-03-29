9e47868264b6b1a6061ab76faeffa3595f0ca05b
I1nur <tuxtarov0475@gmail.com>
2025-03-29 20:02:13 +0400

    feature: add models user and auth

---
+++ src/main/java/http/server/backend/service/interfaces/ISessionService.java
@@ -0,0 +1,20 @@
+package http.server.backend.service.interfaces;
+
+import http.server.backend.model.Session;
+import http.server.backend.model.User;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
+
+public interface ISessionService {
+    Session createSession(User user);
+
+    boolean validateSession(String sessionId);
+
+    void removeSession(String sessionId);
+
+    Long getUserId(String token);
+
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.DAYS)
+    void clearExpiredSessions();
+}
